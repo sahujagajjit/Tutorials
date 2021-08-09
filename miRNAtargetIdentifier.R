72587449f2806e33435ed78b6d889a0a7e7981e7
sahujagajjit <jagajjit_sahu@persistent.com>
2021-08-09 14:55:35 +0530

    Modified for excluding miRNAs for which no targets available...

--- miRNAtargetIdentifier.R
+++ miRNAtargetIdentifier.R
@@ -5,11 +5,13 @@
 
 # Define path 
 #setwd("E:/Others/")
-
-install.packages("BiocManager")
-BiocManager::install("miRNAtap")
-BiocManager::install("miRNAtap.db")
-BiocManager::install("topGO")
+# Installing packages
+#install.packages("BiocManager")
+#BiocManager::install("miRNAtap")
+#BiocManager::install("miRNAtap.db")
+#BiocManager::install("topGO")
+#BiocManager::install("org.Hs.eg.db")
+#BiocManager::install("annotate")
 
 library(dplyr)
 library(miRNAtap)
@@ -18,20 +20,25 @@ library(topGO)
 
 # Create a function for more than one miRNA at a time
 # Input miRNA IDs; Modify based on the input source such as from file
-mirList <- c("miR-10b", "miR-103")
+mirList <- c("miR-10b", "miR-103", "miR-130")
 # Define variable for saving all targets
 allTargts <- rbind()
 for (i in 1:length(mirList)) {
   mir <- mirList[i]
   targts <- getPredictedTargets(mir, species = 'hsa',method = 'geom', min_src = 2)
-  targts <- as.data.frame(targts)
-  # Convert the rownames into a column
-  targts$EntrezIDs <- rownames(targts)
-  # Remove rownames
-  rownames(targts) <- NULL
-  # Add another column for miRNA
-  targts$miRNA <- mir
-  allTargts <- bind_rows(allTargts, targts)
+  if(!is.null(targts)){
+    targts <- as.data.frame(targts)
+    # Convert the rownames into a column
+    targts$EntrezIDs <- rownames(targts)
+    # Remove rownames
+    rownames(targts) <- NULL
+    # Add another column for miRNA
+    targts$miRNA <- mir
+    allTargts <- bind_rows(allTargts, targts)
+  }else{
+    print(paste0("No targets were found for: ", mir))
+  }
+  
 }
 # Arrange columns
 allTargts <- allTargts[, c(ncol(allTargts), (ncol(allTargts)-1), 1:(ncol(allTargts)-2))]
@@ -39,8 +46,6 @@ allTargts <- allTargts[, c(ncol(allTargts), (ncol(allTargts)-1), 1:(ncol(allTarg
 #write.csv(allTargts, file = "allTargts.csv", row.names = F)
 
 # Adding another column as GeneSymbol which should be obtained from the Entrez IDs
-BiocManager::install("org.Hs.eg.db")
-BiocManager::install("annotate")
 library(org.Hs.eg.db)
 library(annotate)
 
